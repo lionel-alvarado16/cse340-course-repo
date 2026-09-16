9587551bb9edd1cfb938b2c2c16b884e3db527c8
lionel-alvarado16 <lalvaradoanastacio@byupathway.edu>
2026-09-16 17:24:25 -0500

    db ssl update

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,8 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    // ssl: true
+    ssl: process.env.DB_URL?.includes('render.com') ? { rejectUnauthorized: false } : false
 });
 
 /**
